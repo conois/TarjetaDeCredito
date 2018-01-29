20b58d6b63bb8af3cf3ab6dad583b155174a9607
conois <conii.echeverria@gmail.com>
2018-01-29 10:02:19 -0300

    con eslist

--- index.js
+++ index.js
@@ -1,48 +1,44 @@
-var btn= document.getElementById('btnPagar');
-btn.addEventListener('click', isValidCard);
-
-
-function isValidCard(){
-  var numbercard= document.getElementById("cn").value;
+function isValidCard() {
+  var numbercard = document.getElementById('cn').value;
   if (numbercard.length === 0) {
-      console.log("error"); 
-    } else {
-      var arr = [];
-      for (var i = 0; i < numbercard.length; i++) {
-        arr.push(numbercard[i]);
-      } for (var elem = 0; elem < arr.length; elem++) {
-        var item = arr.pop();
-        arr.splice(elem, 0, item);
-        var num = [];
-      } for (var x = 0; x < arr.length; x++) {
-        num.push(parseInt(arr[x]));
-        var pairPosition = [];
-        var impairPosition = 0;
-        var result;
+    console.log('error');
+  } else {
+    var arr = [];
+    for (var i = 0; i < numbercard.length; i++) {
+      arr.push(numbercard[i]);
+    } for (var elem = 0; elem < arr.length; elem++) {
+      var item = arr.pop();
+      arr.splice(elem, 0, item);
+      var num = [];
+    } for (var x = 0; x < arr.length; x++) {
+      num.push(parseInt(arr[x]));
+      var pairPosition = [];
+      var impairPosition = 0;
+      var result;
       // Separar los números en posiciones pares
-      } for (var p = 0; p < num.length; p ++) {
-        if ([ p + 1] % 2 === 0) {
-          pairPosition.push((num[p]) * 2);
-        } else if ([p + 1] % 2 !== 0)
-          impairPosition += (num[p]);
-        var pairPositionLess = 0;
-        var pairPositionHigth = [];
-      } for (var s = 0; s < pairPosition.length; s++) {
-        if (pairPosition[s] < 10) {
-          pairPositionLess += (pairPosition[s]);
-        } else {
-          pairPositionHigth.push(pairPosition[s]);
-          var pairString = pairPositionHigth.join('');
-          var sum = 0;
-          for (var y = 0; y < pairString.length; y++) {
-            sum += parseInt(pairString[y]);
-          }
+    } for (var pe = 0; pe < num.length; pe ++) {
+      if ([ pe + 1] % 2 === 0) {
+        pairPosition.push((num[pe]) * 2);
+      } else if ([pe + 1] % 2 !== 0)
+        impairPosition += (num[pe]);
+      var pairPositionLess = 0;
+      var pairPositionHigth = [];
+    } for (var se = 0; se < pairPosition.length; se++) {
+      if (pairPosition[se] < 10) {
+        pairPositionLess += (pairPosition[se]);
+      } else {
+        pairPositionHigth.push(pairPosition[se]);
+        var pairString = pairPositionHigth.join('');
+        var sum = 0;
+        for (var ye = 0; ye < pairString.length; ye++) {
+          sum += parseInt(pairString[ye]);
         }
       }
-    }result = sum + impairPosition + pairPositionLess;
-    if (result % 10 === 0) {
-      alert('Tarjeta Válida');
-    } else {
-      alert('Tarjeta Inválida');
     }
+  }result = sum + impairPosition + pairPositionLess;
+  if (result % 10 === 0) {
+    alert('Tarjeta Válida');
+  } else {
+    alert('Tarjeta Inválida');
+  }
 }
